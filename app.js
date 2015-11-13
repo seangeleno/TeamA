5ac15de2e4771a921f70f980063174a537ee689b
Sean Esteva <seangeleno@gmail.com>
2015-11-13 12:43:57 -0800

    added rebeccapurple and the homepage

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 var express = require('express');
 var app     = express();
 
+app.get('/',function(req, res){
+  res.send('<h1 style="text-align:center;font-family:Impact;color:rebeccapurple">Welcome to the homepage!</h1>')
+})
+
+
 app.listen(3000, function(){
   console.log('Server running on port 3000')
 })
