2168947d9ac15abe70f5b468804456e314f4142f
Colin <clnsdabst@gmail.com>
2015-11-15 19:11:09 -0800

    added required package depencies and made changes to app.js

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-//
-var express = require('express');
-var app     = express();
+var express = require('express'),
+        app = express(),
+     logger = require('morgan'),
+ bodyParser = require('body-parser'),
+   mongoose = require('mongoose')
 
+//establish connection to MongoDB
+mongoose.connect('mongodb://localhost/')
+
+//log requests made to app
+app.use(logger('dev'))
+
+//make json object available in requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+//create root route
 app.get('/',function(req, res){
-  res.send('<h1 style="text-align:center;font-family:Impact;color:rebeccapurple">Welcome to the homepage!</h1>')
+  res.send('<h1 style="text-align:center;font-family:Avenir;color:rebeccapurple;background-color:lightgray;font-weight:200">Welcome to the homepage!</h1>')
 })
 
 
