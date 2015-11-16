a3e084e72b12f6eb0c5f7c97c59430289c2f7c04
Colin <clnsdabst@gmail.com>
2015-11-16 12:52:26 -0800

    added functionality to app.js for ejs and fixed titles routes

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var express = require('express'),
         app = express(),
      logger = require('morgan'),
  bodyParser = require('body-parser'),
-   mongoose = require('mongoose')
+   mongoose = require('mongoose'),
+				ejs = require('ejs'),
+ ejsLayouts = require('express-ejs-layouts')
+
+//app configurations
+app.set('view engine', 'ejs')
 
 //establish connection to MongoDB
 mongoose.connect('mongodb://localhost/')
@@ -19,7 +24,9 @@ app.get('/',function(req, res){
   res.send('<h1 style="text-align:center;font-family:Avenir;color:rebeccapurple;background-color:lightgray;font-weight:200">Welcome to the homepage!</h1>')
 })
 
-
+//import routes
+var gameRoutes = require('./routes/game_routes.js')
+app.use('/games', gameRoutes)
 app.listen(3000, function(){
   console.log('Server running on port 3000')
 })
