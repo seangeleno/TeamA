5e0f32e82f42478b1792ebe3e3431863ad602eff
Sean Esteva <seangeleno@gmail.com>
2015-11-15 19:44:38 -0800

    added module.exports, linked the controller to the model via Game var

---
+++ controllers/game_controller.js
@@ -0,0 +1,22 @@
+var Game = require('../models/game.js')
+
+function index(req, res) {
+
+}
+
+//first we have create, empty brackets signify all games
+function create(){}
+
+function show(){}
+
+function update(){}
+
+function delete(){}
+
+module.exports = {
+  index: index,
+  create: create,
+  show: show,
+  update: update,
+  delete: delete
+}
